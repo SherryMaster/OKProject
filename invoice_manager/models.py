4e111c494911f3000d8020810d5ab5940c1012cf
SherryMaster <sherrymaster2@gmail.com>
2024-10-18 14:31:46 +0500

    invoice and customer models are created

--- invoice_manager/models.py
+++ invoice_manager/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Invoice(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    amount = models.FloatField()
+    amount_paid = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.customer.name + ' - ' + str(self.amount_paid) + '/' + str(self.amount)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
