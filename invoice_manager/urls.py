b7e2f3aa1a468ec80751e85252b591f102ec4fa0
SherryMaster <sherrymaster2@gmail.com>
2024-10-20 14:24:43 +0500

    V4

--- invoice_manager/urls.py
+++ invoice_manager/urls.py
@@ -7,5 +7,6 @@ app_name = "invoice_manager"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("items", ItemsView.as_view(), name="items"),
-    path("items/<int:pk>/delete", ItemDeleteView.as_view(), name="item_delete"),
+    path("items/<int:pk>/delete", item_delete, name="item_delete"),
+    path("items/<int:pk>/update", item_update, name="item_update"),
 ]
--- invoice_manager/views.py
+++ invoice_manager/views.py
@@ -1,6 +1,26 @@
+from django.http import JsonResponse
 from django.views.generic import TemplateView, CreateView, DeleteView, UpdateView
 from .models import Invoice, Customer, Item
 
+
+def item_delete(request, pk):
+    if request.method == "POST":
+        item = Item.objects.get(pk=pk)
+        item.delete()
+        return JsonResponse({"success": True})
+
+    return JsonResponse({"success": False})
+
+
+def item_update(request, pk):
+    if request.method == "POST":
+        item = Item.objects.get(pk=pk)
+        item.name = request.POST.get("name")
+        item.rate = request.POST.get("rate")
+        item.save()
+        return JsonResponse({"success": True, "item": {"name": item.name, "rate": item.rate}})
+
+    return JsonResponse({"success": False})
 
 class IndexView(TemplateView):
     template_name = "invoice_manager/index.html"
@@ -16,16 +36,6 @@ class ItemsView(CreateView):
     template_name = "invoice_manager/item_list.html"
     model = Item
     fields = ["name", "rate"]
-    success_url = "/items"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["items"] = Item.objects.all()
-        return context
-
-
-class ItemDeleteView(DeleteView):
-    model = Item
     success_url = "/items"
 
     def get_context_data(self, **kwargs):
